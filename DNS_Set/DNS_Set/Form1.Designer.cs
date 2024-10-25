c27293e0b981d5ed8b8915fcc79cd005d965500d
mohammadalighaderi72 <107200374+mohammadalighaderi72@users.noreply.github.com>
2024-10-25 07:35:30 +0330

    Add files via upload

---
+++ DNS_Set/DNS_Set/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace DNS_Set
+{
+    partial class FrmDNS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDNS));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(476, 107);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(332, 34);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(135, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "شبکه را انتخاب کنید";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(314, 77);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "تنظیم DNS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox1.Location = new System.Drawing.Point(476, 238);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 35);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "178.22.122.100";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox2.Location = new System.Drawing.Point(476, 354);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(7);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(332, 35);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "185.51.200.2";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(665, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 42);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "نام شبکـــــه:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 27);
+            this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(321, 42);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "آدرس DNS شبکه انتخاب شده";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(79, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(314, 77);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "خروج";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(615, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 42);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "آدرس DNS اولیه";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(605, 302);
+            this.label5.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 42);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "آدرس DNS ثانویه";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(79, 250);
+            this.button3.Margin = new System.Windows.Forms.Padding(7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(314, 77);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "تنظیم DNS پیش فرض";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // FrmDNS
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(903, 420);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("B Zar", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(10, 8, 10, 8);
+            this.MaximizeBox = false;
+            this.Name = "FrmDNS";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "تنظیم سادۀ دی ان اس";
+            this.Load += new System.EventHandler(this.FrmDNS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DNS_Set/DNS_Set/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.IO;
+namespace DNS_Set
+{
+    public partial class FrmDNS : Form
+    {
+        public FrmDNS()
+        {
+
+
+            InitializeComponent();
+        }
+
+        private void FrmDNS_Load(object sender, EventArgs e)
+        {
+
+
+
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+
+            if (!isAdmin)
+
+            {
+                MessageBox.Show("برنامه باید با سطح دسترسی ادمین اجرا شود.", "خطا در اجرای برنامه", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+
+
+            foreach (NetworkInterface adapter in adapters)
+            {
+                comboBox1.Items.Add(adapter.Name);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // دریافت نام آداپتر انتخاب شده
+                string selectedAdapterName = comboBox1.SelectedItem.ToString();
+
+                // پیدا کردن آداپتر انتخاب شده
+                NetworkInterface adapter = NetworkInterface.GetAllNetworkInterfaces()
+                    .FirstOrDefault(a => a.Name == selectedAdapterName);
+
+                if (adapter != null)
+                {
+                    // دریافت آدرس های IP و DNS آداپتر
+                    IPInterfaceProperties ipProperties = adapter.GetIPProperties();
+                    IPAddressCollection dnsAddresses = ipProperties.DnsAddresses;
+
+                    // نمایش آدرس DNS در Label
+                    label1.Text = dnsAddresses.FirstOrDefault().ToString();
+                }
+                else
+                {
+                    label1.Text = "آداپتر انتخاب شده یافت نشد.";
+                }
+
+
+            }
+            catch
+            {
+                MessageBox.Show("خطا در انجام عملیات مجدد تلاش کنید", "عملیات ناموفق", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectedAdapterName = comboBox1.SelectedItem.ToString();
+                
+                
+                string filetext =     "netsh interface ipv4 set dnsservers name=\""+ selectedAdapterName + "\" source=dhcp \n " + "netsh interface ipv4 add dnsserver \"" + selectedAdapterName + "\" " + textBox1.Text + "\n" +"netsh interface ipv4 add dnsserver \"" + selectedAdapterName + "\" " + textBox2.Text;
+                File.WriteAllText("network.bat", filetext);
+
+                // دریافت مسیر اجرای برنامه
+                string currentDirectory = Directory.GetCurrentDirectory();
+
+                // تشکیل مسیر کامل فایل bat
+                string batchFilePath = Path.Combine(currentDirectory, "network.bat");
+
+                // اجرای فایل bat با استفاده از کد قبلی
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + batchFilePath;
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+
+                Process process = new Process();
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+                File.Delete("network.bat");
+                MessageBox.Show("آدرس DNS با موفقیت تغییر یافت.", "عملیات موفق", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("خطا در انجام عملیات مجدد تلاش کنید", "عملیات ناموفق", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectedAdapterName = comboBox1.SelectedItem.ToString();
+
+
+                string filetext = "netsh interface ipv4 set dnsservers name=\"" + selectedAdapterName + "\" source=dhcp \n " + "ipconfig /renew";  
+                File.WriteAllText("network.bat", filetext);
+
+                // دریافت مسیر اجرای برنامه
+                string currentDirectory = Directory.GetCurrentDirectory();
+
+                // تشکیل مسیر کامل فایل bat
+                string batchFilePath = Path.Combine(currentDirectory, "network.bat");
+
+                // اجرای فایل bat با استفاده از کد قبلی
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + batchFilePath;
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+
+                Process process = new Process();
+                process.StartInfo = startInfo;
+                process.Start();
+
+                process.WaitForExit();
+                File.Delete("network.bat");
+                MessageBox.Show("آدرس DNS با موفقیت تغییر یافت.", "عملیات موفق", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("خطا در انجام عملیات مجدد تلاش کنید", "عملیات ناموفق", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+    
+}
